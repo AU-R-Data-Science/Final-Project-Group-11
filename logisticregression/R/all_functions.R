a5d98e02dc693f203e270caa3e90b82db26f9b6b
Erica Maul <emaul@wellesley.edu>
2022-12-02 11:55:01 -0600

    Update all_functions.R

--- logisticregression/R/all_functions.R
+++ logisticregression/R/all_functions.R
@@ -1,8 +1,7 @@
 #' @title Initial Beta
 #' @description Calculate an initial beta value that will be used as the parameter of the optimization function for beta hat, using a least squares equation.
 #' @param X An initial \code{matrix} to be used in the function.
-#' @param y A \code{vector} of y values to be used in the function.
-#' @param B A \code{numeric} (integer) used to denote the number of simulations.
+#' @param y A \code{vector} of y values to be used in the function. simulations.
 #' @return A \code{matrix} containing the initial beta value(s).
 initial_beta<- function(X,y){
   beta = solve(t(X)%*%X)%*%t(X)%*%y
@@ -16,7 +15,8 @@ initial_beta<- function(X,y){
 #' @param beta The \code{matrix} of initial beta values produced in the Initial Beta function.
 #' @return A \code{numeric} of a value to be optimized.
 loss_func <-function(y,X,beta){
-  for (i in 1:10){
+  p <- numeric(0)
+  for (i in 1:rows){
     p[i] <- 1/(1+exp(-t(X[i,])%*%beta))
   }
   loss_func = sum(-y*log(p)-(1-y)*log(1-p))
@@ -25,12 +25,22 @@ loss_func <-function(y,X,beta){
 
 #' @title Beta Hat
 #' @description Calculate the optimized beta hat value.
-#' @param beta The \code{matrix} of initial beta values
 #' @param X An initial \code{matrix} to be used in the function.
 #' @param y A \code{vector} of y values to be used in the function.
 #' @return A \code{matrix} containing the resulting beta hat value(s).
-beta_hat <- function(beta, X, y){
-  optim(par=beta,fn=loss_func,X=X,y=y)$par
+beta_hat<-function(X,y){
+  beta_hat=optim(par=initial_beta(X,y),fn=loss_func,X=X,y=y)$par
+  return(beta_hat)
+}
+
+#' @title Data
+#' @description Create a data frame from the X matrix and y vector.
+#' @param y A \code{vector} of y values to be used in the function.
+#' @param X A \code{matrix} to be used in the function.
+#' @return A \code{dataframe} of the inputted data.
+data<-function(y,X){
+  data = data.frame(y, X)
+  return(data)
 }
 
 #' @title Bootstrap
@@ -40,19 +50,24 @@ beta_hat <- function(beta, X, y){
 #' @param alpha A \code{numeric} inputted by the user to find desired quantiles.
 #' @param B A \code{numeric} describing the desired amount of bootstrap iterations.
 #' @return A \code{matrix} containing the calculated quantiles.
-bootstrap <- function(X,y,alpha,B=20){
-  data = data.frame("y" = y, X)
+bootstrap <- function(data=data(y,X),alpha,B=20){
+
   beta_mat = matrix(NA, nrow = B, ncol = dim(X)[2])
   for(b in 1:B){
     boot_data = data[sample(1:nrow(data), nrow(data), replace = TRUE), ]
-    beta_mat[b, ] = optim(par=beta,fn=loss_func,X=boot_data[,c(2:ncol(boot_data))],y=boot_data[,1])$par
+
+    #rename the rows of boot_data to 1,2,3,... This is so that in loss_func, the iterations are okay
+    row.names(boot_data) = 1:100
+
+    beta_mat[b, ] = optim(par=initial_beta(X,y),fn=loss_func,X=as.matrix(boot_data[,c(2:ncol(boot_data))]),y=boot_data[,1])$par
+
   }
   alpha=0.05
   quantiles <- matrix(NA,nrow=ncol(beta_mat),2)
   for (i in 1:ncol(beta_mat)){
     quantiles[i,]=quantile(beta_mat[,i], c(alpha/2, 1 - alpha/2))
   }
-  rownames(quantiles) <- c(seq(0,ncol(X)-1,by = 1))
+  rownames(quantiles) = sprintf("B%d", 0:(nrow(quantiles)-1)) #uses C string formatting
   colnames(quantiles) <- c(alpha/2,1-alpha/2)
   return(quantiles)
 }
@@ -64,7 +79,7 @@ bootstrap <- function(X,y,alpha,B=20){
 #' @param beta_hat A \code{matrix} of the optimized beta values.
 #' @param cutoff A \code{numeric} specified by the user for a cutoff value.
 #' @return A \code {list} of the calculated metrics.
-metrics <- function(X=X,beta=beta,beta_hat=beta_hat,cutoff=0.5){
+metrics <- function(X=X,beta=initial_beta(X,y),beta_hat=beta_hat(),cutoff=0.5){
   p <- numeric()
   p_predicted <- numeric()
 
@@ -106,11 +121,21 @@ metrics <- function(X=X,beta=beta,beta_hat=beta_hat,cutoff=0.5){
     "Specificity"=Specificity,"False_Discovery_Rate"=False_Discovery_Rate,
     "Diagnostic_Odds_Ratio"=Diagnostic_Odds_Ratio)
 }
-cutoffs <- seq(0.1,0.9,0.1)
-metricsmat <- as.data.frame(matrix(NA,nrow = 6,ncol = length(cutoffs)))
 
-for (i in 1:length(cutoffs)){
-  metricsmat[,i]<-metrics(X=X,beta=beta,beta_hat=beta_hat,cutoff = cutoffs[i])
+#' @title Metrics Plot
+#' @description Plot a user-assigned metric on a grid.
+#' @param metric A \code{character} assigning which metric to plot.
+#' @return A plot showing the desired matrix on a grid.
+metricsplot <- function(metric){
+  cutoffs <- seq(0.1,0.9,0.1)
+  metricsmat <- as.data.frame(matrix(NA,nrow = 6,ncol = length(cutoffs)))
+
+  for (i in 1:length(cutoffs)){
+    metricsmat[,i]<-metrics(X=X,beta=initial_beta(X,y),beta_hat=beta_hat(),cutoff = cutoffs[i])
+  }
+  rownames(metricsmat) <- names(metrics(X=X,beta=initial_beta(X,y),beta_hat=beta_hat(),cutoff = 0.5))
+  colnames(metricsmat) <- cutoffs
+  plot(y=metricsmat[metric,],x=cutoffs,xlab="Cut-offs",ylab=metric)
 }
 
 #' @title Logistic Curve
@@ -119,7 +144,7 @@ for (i in 1:length(cutoffs)){
 #' @param predictor A \code{numeric} to assign the predictor variable
 #' @param beta_hat A \code{matrix} of the optimized beta values.
 #' @return A plot depicting a fitted logistic curve to the actual values.
-logistic_curve = function(data, predictor, beta_hat) {
+logistic_curve = function(data=data(y,X), predictor, beta_hat=beta_hat()) {
 
   #find out which beta to use
   index = which(colnames(data) == predictor)
