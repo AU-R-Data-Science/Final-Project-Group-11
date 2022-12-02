e0e2b7d5c2843e0f7d269fa76aa6477548b6fdcf
rukeshgusain0072 <rzg0072@auburn.edu>
2022-12-02 12:09:12 -0600

    Update all_functions.R

--- logisticregression/R/all_functions.R
+++ logisticregression/R/all_functions.R
@@ -15,8 +15,9 @@ initial_beta<- function(X,y){
 #' @param X An initial \code{matrix} to be used in the function.
 #' @param beta The \code{matrix} of initial beta values produced in the Initial Beta function.
 #' @return A \code{numeric} of a value to be optimized.
-loss_func <-function(y,X,beta){
-  for (i in 1:10){
+loss_func <-function(y,X,beta=initial_beta(X,y)){
+  p <- numeric(0)
+  for (i in 1:length(y)){
     p[i] <- 1/(1+exp(-t(X[i,])%*%beta))
   }
   loss_func = sum(-y*log(p)-(1-y)*log(1-p))
@@ -29,8 +30,17 @@ loss_func <-function(y,X,beta){
 #' @param X An initial \code{matrix} to be used in the function.
 #' @param y A \code{vector} of y values to be used in the function.
 #' @return A \code{matrix} containing the resulting beta hat value(s).
-beta_hat <- function(beta, X, y){
-  optim(par=beta,fn=loss_func,X=X,y=y)$par
+################################################################################
+beta_hat<-function(){
+  beta_hat=optim(par=initial_beta(X,y),fn=loss_func,X=X,y=y)$par
+  return(beta_hat)
+}
+
+################################################################################
+
+data<-function(y,X){
+  data = data.frame(y, X)
+  return(data)
 }
 
 #' @title Bootstrap
@@ -57,61 +67,6 @@ bootstrap <- function(X,y,alpha,B=20){
   return(quantiles)
 }
 
-#' @title Metrics
-#' @description Calculate a number of statistical metrics from a confusion matrix comparing predicted and actual data.
-#' @param beta The \code{matrix} of initial beta values produced in the Initial Beta function.
-#' @param X A \code{matrix} from which data will be pulled to fit the model.
-#' @param beta_hat A \code{matrix} of the optimized beta values.
-#' @param cutoff A \code{numeric} specified by the user for a cutoff value.
-#' @return A \code {list} of the calculated metrics.
-metrics <- function(X=X,beta=beta,beta_hat=beta_hat,cutoff=0.5){
-  p <- numeric()
-  p_predicted <- numeric()
-
-  for (i in 1:10){
-    p_predicted[i] <- 1/(1+exp(-t(X[i,])%*%beta_hat))
-    if(p_predicted[i]<=cutoff)p_predicted[i]=0
-    else p_predicted[i]=1
-  }
-
-  for (i in 1:10){
-    p[i] <- 1/(1+exp(-t(X[i,])%*%beta))
-    if(p[i]<=cutoff)p[i]=0
-    else p[i]=1
-  }
-  TP=0
-  TN=0
-  FP=0
-  FN=0
-  confusionmatrix<-rbind(p,p_predicted)
-  for (i in 1:ncol(confusionmatrix)){
-    if (confusionmatrix["p",i]==1 && confusionmatrix["p_predicted",i]==1) TP=TP+1
-    if(confusionmatrix["p",i]==1 && confusionmatrix["p_predicted",i]==0) FN=FN+1
-    if(confusionmatrix["p",i]==0 && confusionmatrix["p_predicted",i]==1) FP=FP+1
-    if(confusionmatrix["p",i]==0 && confusionmatrix["p_predicted",i]==0) TN=TN+1
-  }
-  Prevalence <- (FN+TP)/((FN+TP)+(FP+TN))
-  Accuracy <- (TP+TN)/(TP+TN+FP+FN)
-  Sensitivity <- TP/(TP+FN)
-  Specificity <- TN/(TN+FP)
-  False_Discovery_Rate <- FP/(FP+TP)
-  TPR <- Sensitivity
-  TNR <- Specificity
-  FPR <- 1-TNR
-  FNR <- 1-TPR
-  LRp <- TPR/FPR
-  LRn <- FNR/TNR
-  Diagnostic_Odds_Ratio <- LRp/LRn
-  c("Prevalence"=Prevalence,"Accuracy"=Accuracy,"Sensitivity"=Sensitivity,
-    "Specificity"=Specificity,"False_Discovery_Rate"=False_Discovery_Rate,
-    "Diagnostic_Odds_Ratio"=Diagnostic_Odds_Ratio)
-}
-cutoffs <- seq(0.1,0.9,0.1)
-metricsmat <- as.data.frame(matrix(NA,nrow = 6,ncol = length(cutoffs)))
-
-for (i in 1:length(cutoffs)){
-  metricsmat[,i]<-metrics(X=X,beta=beta,beta_hat=beta_hat,cutoff = cutoffs[i])
-}
 
 #' @title Logistic Curve
 #' @description Plot the logistic regression of the data.
@@ -143,13 +98,13 @@ metrics <- function(X=X,beta=initial_beta(X,y),beta_hat=beta_hat(),cutoff=0.5){
   p <- numeric()
   p_predicted <- numeric()
 
-  for (i in 1:nrow(y)){
+  for (i in 1:length(y)){
     p_predicted[i] <- 1/(1+exp(-t(X[i,])%*%beta_hat))
     if(p_predicted[i]<=cutoff)p_predicted[i]=0
     else p_predicted[i]=1
   }
 
-  for (i in 1:nrow(y)){
+  for (i in 1:length(y)){
     p[i] <- 1/(1+exp(-t(X[i,])%*%beta))
     if(p[i]<=cutoff)p[i]=0
     else p[i]=1
